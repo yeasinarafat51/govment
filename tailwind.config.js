64027656e62ff50f1ca26ad6cf7b2808369b0999
yeasinarafat51 <aloondokar65@gmail.com>
2024-10-11 23:22:23 +0600

    customstyle

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#00D2FF',
+        secondary:'#2D2D2D',
+        accent:'#666666'
+ },
+//  background: linear-gradient(180deg, #00D2FF 0%, #059DBE 100%);
+
+  backgroundImage:{
+     "gradient-primary": "linear-gradient(180deg, #00D2FF 0%, #059DBE 100%)",
+        "gradient-secondary":
+          "linear-gradient(161.45deg, #00D2FF -5.54%, #9AEDFF 59.11%)",
+
+
+  },
+      fontFamily: {
+        "space-grotesk": ['"Space Grotesk"', "sans-serif"],
+        "nunito-sans": ['"Nunito Sans"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
+
 
